f9eeda9984682d2938fafd7a4095fa7c68cce1df
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-04-27 15:50:17 -0700

    heros-component-unit-test-with-mocks

---
+++ src/app/heroes/heros.component.spec.ts
@@ -0,0 +1,31 @@
+import { of } from "rxjs";
+import { HeroesComponent } from "./heroes.component"
+
+describe('HerosComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroService;
+
+  beforeEach(() => {
+    HEROES = [
+      {id:1, name: 'SpiderDude', stregth: 8},
+      {id:2, name: 'Wonderful Woman', stregth: 24},
+      {id:3, name: 'SuperDude', stregth: 55}
+    ]
+
+    mockHeroService = jasmine.createSpyObj(['getHeros', 'addHero', 'deleteHero'])
+
+    component = new HeroesComponent(mockHeroService);
+  })
+
+  describe('delete', () => {
+    it('should remove the indictaed hero from the heros list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true))
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+    })
+  })
+})
