741f994d7cc2007e88abacc128c49ed42c1fa042
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-05-04 09:39:10 -0700

    mocking-child-component

--- src/app/heroes/heros.component.shallow.spec.ts
+++ src/app/heroes/heros.component.shallow.spec.ts
@@ -1,12 +1,24 @@
-import { Component, NO_ERRORS_SCHEMA } from "@angular/core";
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { of } from "rxjs";
+import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
-import { HeroComponent } from "../hero/hero.component";
+import { HeroesComponent } from "./heroes.component";
 
 describe('HerosComponent (shallow test)', () => {
-  let fixture: ComponentFixture<HeroComponent>;
+  let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
+
+  // @Component({
+  //   selector: 'app-hero',
+  //   template: '<div></div>'
+  // })
+  // class HeroComponent {
+  //   @Input() hero: Hero;
+  //   // @Output() delete = new EventEmitter();
+  // }
 
   beforeEach(() => {
     HEROES = [
@@ -17,17 +29,26 @@ describe('HerosComponent (shallow test)', () => {
     mockHeroService = jasmine.createSpyObj(['getHeros', 'addHero', 'deleteHero']);
 
     TestBed.configureTestingModule({
-      declarations: [HeroComponent],
+      declarations: [HeroesComponent],
       providers: [
         { provide: HeroService, useValue: mockHeroService }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
-    fixture = TestBed.createComponent(HeroComponent);
+    fixture = TestBed.createComponent(HeroesComponent);
   });
 
   it('should heros correctly from service', () => {
-    mockHeroService.getHeros.and.return
-    expect(true).toBe(true);
-  })
+    mockHeroService.getHeroes.and.returnValue(of(HEROES))
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+  // it('should create one li for each hero', () => {
+  //   mockHeroService.getHeroes.and.returnValue(of(HEROES))
+  //   fixture.detectChanges();
+
+  //   expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+  // })
 })
