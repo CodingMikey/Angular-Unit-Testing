dcd2a05b408200dce2559676f16e40d50b5d3fb3
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-05-07 16:21:09 -0500

    getHero-unit-test

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -2,10 +2,13 @@ import { TestBed } from "@angular/core/testing";
 import { HeroService } from "./hero.service";
 import { MessageService } from "./message.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { inject } from "@angular/core";
 
 describe('HeroService', () => {
     let mockMessageService;
     let httpTestingController: HttpTestingController;
+    let service: HeroService;
+
     beforeEach(() => {
       mockMessageService = jasmine.createSpyObj(['add']); 
       
@@ -18,6 +21,16 @@ describe('HeroService', () => {
       })
 
       httpTestingController = TestBed.get(HttpTestingController);
-      let msgSvc = TestBed.get(MessageService);
+    });
+
+    describe('getHero', () => {
+        
+        it('should call get with the correct URL', () => {
+            service.getHero(4).subscribe();
+
+            const req = httpTestingController.expectOne('api/heroes/4');
+            req.flush({id: 4, name: 'SuperDude', strength: 100});
+            httpTestingController.verify();
+        })
     })
 })
